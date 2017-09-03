d5fc20b4be8f7edd764eea78b174db3f1b437f64
azbest <flaxazbest@gmail.com>
2017-09-04 00:22:57 +0300

    Анотація з параметрами

--- src/App.java
+++ src/App.java
@@ -3,22 +3,26 @@ import java.lang.reflect.Method;
 public class App {
 
     public static void main(String[] args) {
-        App app = new App();
-
-        try {
-            Class<?> c = app.getClass();
-            Method m = c.getMethod("method");
-            MyAnnotation annotation = m.getAnnotation(MyAnnotation.class);
-            System.out.println(annotation.description());
-        }
-        catch (NoSuchMethodException e) {
-            System.out.println("Метод не знайдено");
-        }
+        method2("Тест", 300);
     }
 
     @MyAnnotation(description = "Опис методу")
     public static void method() {
 
+    }
+
+    @Anno(descr = "Два параметра", val = 4)
+    public static void method2(String s, int i) {
+        App app = new App();
+        try {
+            Class<?> c = app.getClass();
+            Method m = c.getMethod("method2", String.class, int.class);
+            Anno anno = m.getAnnotation(Anno.class);
+            System.out.println(anno.descr() + " " + anno.val());
+        }
+        catch (NoSuchMethodException e) {
+            System.err.println(e);
+        }
     }
 
 }
